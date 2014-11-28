a82360d311f2905d7499e481004d48d341750384
Jia Sui <jsfaint@gmail.com>
2014-11-28 15:08:54 +0800

    Update neobundle.vim

--- build_ycm.sh
+++ build_ycm.sh
@@ -9,6 +9,3 @@ fi
 cd ./vimfiles/bundle/youcompleteme/
 git submodule update --init --recursive
 ./install.sh --clang-completer --system-libclang
-
-#--clang-completer #With semantic support for C-family language
-#--omnisharp-completer #With semantic support for C#
